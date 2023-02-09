1bd1d7e65f28dd08fda295d4505de455837644df
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-02-09 14:15:29 +0200

    Task-02 update

--- js/task-02.js
+++ js/task-02.js
@@ -7,39 +7,47 @@ const ingredients = [
   'Condiments',
 ];
 
-const listItem1 = document.createElement('li');
-listItem1.classList.add('item');
-listItem1.textContent = 'Potatoes';
+const ingredientsList = document.querySelector('#ingredients');
 
-const listItem2 = document.createElement('li');
-listItem2.classList.add('item');
-listItem2.textContent = 'Mushrooms';
+const items = ingredients.map(ingredient => {
+  const listItem = document.createElement('li');
+  listItem.classList.add('item');
+  listItem.textContent = ingredient;
 
-const listItem3 = document.createElement('li');
-listItem3.classList.add('item');
-listItem3.textContent = 'Garlic';
+  return listItem;
+});
 
-const listItem4 = document.createElement('li');
-listItem4.classList.add('item');
-listItem4.textContent = 'Tomatos';
+console.log(items);
 
-const listItem5 = document.createElement('li');
-listItem5.classList.add('item');
-listItem5.textContent = 'Herbs';
+ingredientsList.append(...items);
 
-const listItem6 = document.createElement('li');
-listItem6.classList.add('item');
-listItem6.textContent = 'Condiments';
+// Добавление по одному элементу
 
-const listEl = document.querySelector('#ingredients');
-listEl.prepend(
-  listItem1,
-  listItem2,
-  listItem3,
-  listItem4,
-  listItem5,
-  listItem6,
-);
+// const listItem1 = document.createElement('li');
+// listItem1.classList.add('item');
+// listItem1.textContent = 'Potatoes';
+
+// const listItem2 = document.createElement('li');
+// listItem2.classList.add('item');
+// listItem2.textContent = 'Mushrooms';
+
+// const listItem3 = document.createElement('li');
+// listItem3.classList.add('item');
+// listItem3.textContent = 'Garlic';
+
+// const listItem4 = document.createElement('li');
+// listItem4.classList.add('item');
+// listItem4.textContent = 'Tomatos';
+
+// const listItem5 = document.createElement('li');
+// listItem5.classList.add('item');
+// listItem5.textContent = 'Herbs';
+
+// const listItem6 = document.createElement('li');
+// listItem6.classList.add('item');
+// listItem6.textContent = 'Condiments';
+
+// Загрузка по одному элементу
 
 // listEl.insertBefore(listItem1, listEl.children[0]);
 // listEl.insertBefore(listItem2, listEl.children[1]);
@@ -47,3 +55,15 @@ listEl.prepend(
 // listEl.insertBefore(listItem4, listEl.children[3]);
 // listEl.insertBefore(listItem5, listEl.children[4]);
 // listEl.insertBefore(listItem6, listEl.children[5]);
+
+// Общая загрузка
+
+// const listEl = document.querySelector('#ingredients');
+// listEl.prepend(
+//   listItem1,
+//   listItem2,
+//   listItem3,
+//   listItem4,
+//   listItem5,
+//   listItem6,
+// );
