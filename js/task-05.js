4c8d2430d6999d64b37d412a3a719b7f3688be0a
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-02-11 22:08:51 +0200

    Task-05 done

---
+++ js/task-05.js
@@ -0,0 +1,15 @@
+const textInput = document.querySelector('#name-input');
+const textOutput = document.querySelector('#name-output');
+
+textInput.addEventListener('input', handleInputChange);
+
+function handleInputChange(event) {
+  textOutput.textContent = event.currentTarget.value;
+  if (event.currentTarget.value === '') {
+    textOutput.textContent = 'Anonymous';
+  }
+}
+
+// Напиши скрипт, який під час набору тексту в інпуті input#name-input (подія input),
+// підставляє його поточне значення в span#name - output.
+// Якщо інпут порожній, у спані повинен відображатися рядок "Anonymous".
